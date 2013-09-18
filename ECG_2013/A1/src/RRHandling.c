a887938c5530cd976f2a7ce4361cd508b2b7c367
Jakob Welner <jakob@welner.dk>
2013-09-18 14:22:26 +0200

    adding RRHandling.c

---
+++ ECG_2013/A1/src/RRHandling.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int RRfind(void)
+{
+    printf("Finding peaks for a living!\n");
+    return 0;
+}
+
+int RRdetermine(void)
+{
+    printf("Prove to me that you are indeed a peak!\n");
+    return 0;
+}
+
+int RRsearchback(void)
+{
+    printf("No peaks in sight!.. I guess I should look closer\n");
+    return 0;
+}
