f78b877f9a3d8a89bb5a14edf68445ee652aec96
Jacob Gjerstrup <jacobgjerstrup@hotmail.com>
2013-11-06 15:50:32 +0100

    Updated report

--- ECG_2013/A2/Report/Code/assembly_mwint.asm
+++ ECG_2013/A2/Report/Code/assembly_mwint.asm
@@ -1,46 +1,42 @@
-// input enten array of counter eller 30 separate vaerdier
-LOAD R0 31 					// loading Counter value: 30
-LOAD R1 32 					// Loading Subtractor value: -1
-LOAD R4 memAddress			// Mem Address of first datapoint (array pointer)
-LOAD R2 R4					// getting first value of datapoints
-LOAD R3 33 					// Initialising Sum: 0
+"// Data is supplied by software at two predefined memory locations."
+"// One for the latest sensor input and another for the the datapoint being 30 values old."
+MOVE R3 R0					"// Accumulator"
+MOVE R4 R0                  "// Input data address"
+MOVE R5 R0                  "// temp calculation container"
+MOVE R2 R0                  "// container for old value"
+
+"// Loading latest data and oldest data (30 steps back)"
+LOAD R1 R4      			"//loads value of latest data point"
+SUBI R5 R4 30               "If R5 gets to be lower than R4 it means that the procedure hasn't yet run 30 times"
+BILT R4 R5 14               "// Branch If Larger Than: If (R4 > R5) goto 14"
+MOVE R2 R0                  "// Old value = 0"
+BRCH 17
+LOAD R2 R5                  "// Load datapoint 30 values old"
+
+"// Updating accumulator"
+ADD R3 R3 R1
+SUB R3 R3 R2
+
+"// Dividing by 30'ish"
+MULI R5 R3 4369
+SHRI R5 R4 17
+STRE [output] R5
+ADDI R4 R4 1
+BRCH 8
 
 
-// loop through 30 elements and add them together
-ADD R3 R3 R2 				// Doing the sum
-ADD R0 R0 R1 				// subtracting from counter (adding -1)
-BZERO 21 					// Breaking out. Pointing to division by 30
-ADD R4 1
-LOAD R2 R4
-LOAD R2 nextValue 			// Loading next value from buffer
-BRANCH 8
+"// Instruction set"
+LOAD    [Reg] [Mem]         
+STRE    [Mem] [Reg]         
+MOVE    [Reg] [Reg]         :: Implemented
 
+ADD     [Reg] [Reg] [Reg]   :: implemented
+SUB     [Reg] [Reg] [Reg]   :: implemented
 
+ADDI    [Reg] [Reg] [Num]   :: implemented
+SUBI    [Reg] [Reg] [Num]	:: implemented
+SHRI 	[Reg] [Reg] [Num]   :: implemented
+MULI 	[Reg] [Reg] [Num]   :: implemented
 
-// divide by 30: multiply by 4369 and bitshift >> 17. Close enough
-LOAD R0 34 					// Overwriting counter - Now multiplier: 4369
-LOAD R1 35 					// Overwriting subtractor - Now bitshift value: 17
-MUL R3 R3 R0 				// multiply by 4369
-SHRW R1 R3 					// bitshift R3 >> 17... should be signed
-STORE R3 outputAddress
-HALT
-
-
-
-
-30
--1
-0
-4369
-17
-
-
-
-// Instruction set
-LOAD
-ADD
-STORE
-BRANCH
-BZERO
-SHRW 	// bitshift right
-MUL 	// Multiply
+BILT    [Reg] [Reg] [Num]   // Branch If Larger Than
+BRCH    [Num]               // Branch
